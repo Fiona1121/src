976cf58ab9e9b27cc736de2d6a976da1c657078b
Fiona1121 <b08901158@ntu.edu.tw>
2020-04-30 13:59:42 +0800

    4/30 maze.py finished: __init__(),BFS(); ongoing:BFS_2()

--- Python/maze.py
+++ Python/maze.py
@@ -15,13 +15,20 @@ class Action(IntEnum):
 
 
 class Maze:
-    def __init__(self, filepath):
+    def __init__(self, filepath): # finished
         # TODO : read file and implement a data structure you like
         self.raw_data = pandas.read_csv(filepath).values
         self.nodes = []
-        self.nd_dict = dict()  # key: index, value: the correspond node
-
-
+        self.nd_dict = {}  # key: index, value: the correspond node
+        for x in range(len(self.raw_data)):
+            node = Node(self.raw_data[x][0])
+            self.nodes.append(node)
+            if x not in self.nd_dict.keys():
+                self.nd_dict[x] = node
+            for y in range(1,5):
+                if not np.isnan(self.raw_data[x][y]):
+                    node.setSuccessor(Node(self.raw_data[x][y]),y,self.raw_data[x][4+y])
+        
     def getStartPoint(self):
         if (len(self.nd_dict) < 2):
             print("Error: the start point is not included.")
@@ -34,12 +41,29 @@ class Maze:
     def BFS(self, nd):
         # TODO : design your data structure here for your algorithm
         # Tips : return a sequence of nodes from the node to the nearest unexplored deadend
-        return None
+        queue = []
+        visited = []
+        visited.append(nd)
+        queue.append(nd)
+        while queue:
+            node = queue.pop(0)
+            for n in node.Successors:
+                if n[0] not in visited:
+                    visited.append(n[0])
+                    queue.append(n[0]) 
+        return visited
 
     def BFS_2(self, nd_from, nd_to):
         # TODO : similar to BFS but fixed start point and end point
         # Tips : return a sequence of nodes of the shortest path
-        return None
+        cur_nd = nd_from
+        visited = []
+        visited.append(cur_nd)
+        while cur_nd != nd_to:
+            for n in cur_nd.Successors:
+                if n[0] not in visited:
+                            
+        return visited
 
     def getAction(self, car_dir, nd_from, nd_to):
         # TODO : get the car action
--- Python/node.py
+++ Python/node.py
@@ -35,3 +35,4 @@ class Node:
                 return True
         return False
 
+
